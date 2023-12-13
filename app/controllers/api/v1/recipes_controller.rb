33c00cfa4498c7440829b9c418494273ceb7b88b
devadrsilva <adriano.mssilva1@gmail.com>
2023-12-13 10:46:16 -0300

    rota funcionando no test

---
+++ app/controllers/api/v1/recipes_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::RecipesController < ActionController::API
+
+  def show
+  end
+
+end
--- spec/requests/api_details_recipe_spec.rb
+++ spec/requests/api_details_recipe_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 describe "API Recipe" do
   context "show" do
     it "com sucesso" do
+      user = create(:user, email: 'user@email.com', password: '123456', role: :user)
       recipe_type = RecipeType.create!(name: "Prato Principal")
       recipe = Recipe.create!(title: 'Hamburguer', cook_time: 10,
                     recipe_type: recipe_type,
                     ingredients: 'hamburguer, pão de hamburguer, queijo',
-                    instructions: 'Frite o hamburguer, coloque no pão, coma')
+                    instructions: 'Frite o hamburguer, coloque no pão, coma', user: user)
 
-      get api_v1_recipe_path
+      get "/api/v1/recipes/#{recipe.id}"
 
       expect(response).to have_http_status(200)
     end
